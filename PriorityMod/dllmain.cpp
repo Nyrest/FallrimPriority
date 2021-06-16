1ef8c4d75d9f1b4f9dd1ed961dac0aecc900d043
Erstori <16686147+Erstori@users.noreply.github.com>
2021-06-16 21:41:26 +0800

    Affinity and Wider Script Extender Support

--- PriorityMod/dllmain.cpp
+++ PriorityMod/dllmain.cpp
@@ -1,5 +1,13 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include "dllmain.h"
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+
+unsigned long affinity = 0;
+int priority = HIGH_PRIORITY_CLASS;
+
+toml::parse_result config;
 
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
@@ -17,32 +25,41 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH:
-		int priority;
+		case DLL_PROCESS_ATTACH:
 
-		if (std::filesystem::exists("Data/SKSE/Plugins/PriorityMod.toml"))
-		{
-			auto config = toml::parse_file("Data/SKSE/Plugins/PriorityMod.toml");
-			int value = config["PriorityMod"]["priority"].value_or(4);
-			priority = priority_classes[(value >= 0 && value <= 5) ? value : 4];
-		}
-		else if (std::filesystem::exists("Data/F4SE/Plugins/PriorityMod.toml"))
-		{
-			auto config = toml::parse_file("Data/F4SE/Plugins/PriorityMod.toml");
-			int value = config["PriorityMod"]["priority"].value_or(4);
-			priority = priority_classes[(value >= 0 && value <= 5) ? value : 4];
-		}
-		else
-		{
-			priority = HIGH_PRIORITY_CLASS;
-		}
-		SetPriorityClass(GetCurrentProcess(), priority);
-		break;
-	case DLL_THREAD_ATTACH:break;
-	case DLL_THREAD_DETACH:break;
-	case DLL_PROCESS_DETACH:break;
+			if (ParseConfig())
+			{
+				int priorityValue = config["PriorityMod"]["priority"].value_or(4);
+				if (priorityValue != -1)
+					priority = priority_classes[(priorityValue >= 0 && priorityValue <= 5) ? priorityValue : 4];
+				affinity = config["PriorityMod"]["affinity"].value_or(0);
+			}
+
+			if (priority != -1)
+				SetPriorityClass(GetCurrentProcess(), priority);
+			if (affinity != 0)
+				SetProcessAffinityMask(GetCurrentProcess(), affinity);
+			break;
+		case DLL_THREAD_ATTACH:break;
+		case DLL_THREAD_DETACH:break;
+		case DLL_PROCESS_DETACH:break;
 	}
 	return TRUE;
 }
 
 
+bool ParseConfig()
+{
+	LPTSTR buffer = new TCHAR[_MAX_PATH];
+	GetModuleFileNameW((HINSTANCE)&__ImageBase, buffer, _MAX_PATH);
+
+	std::wstring filename = buffer;
+
+	std::wstring configPath = filename.substr(0, filename.find_last_of('.')) + L".toml";
+	if (std::filesystem::exists(configPath))
+	{
+		config = toml::parse_file(configPath);
+		return true;
+	}
+	return false;
+}
