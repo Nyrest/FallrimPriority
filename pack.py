9bb149dc61f940d77cd06a1e961e5d91c1225c64
Nyrest <16686147+Nyrest@users.noreply.github.com>
2023-03-25 13:07:30 +0800

    Delete ./tmp after completed

--- pack.py
+++ pack.py
@@ -2,27 +2,30 @@ import os
 import shutil
 import threading
 
-# 定义编译指令
+# Define the compile command
 compile_cmd = "cargo build -r --features \"{flag}\" --target-dir \"./tmp/{game_name}\""
-# 定义目标数组
+# Define the targets array
 targets = [
     ["Skyrim AE", "x86_64-pc-windows-msvc", "skyrim_ae"],
     ["Fallout 4", "x86_64-pc-windows-msvc", "fallout_4"]
 ]
 
-# 创建 "./tmp" 和 "./out" 文件夹
+# Create "./tmp" and "./out" folders
 if not os.path.exists("./tmp"):
     os.makedirs("./tmp")
 if not os.path.exists("./out"):
     os.makedirs("./out")
 
-# 定义函数来运行编译指令
+# Define a function to run the compile command
+
+
 def compile_target(target):
     game_name, arch, flag = target
     cmd = compile_cmd.format(game_name=game_name, flag=flag)
     os.system(cmd)
 
-# 多线程并行遍历数组来编译目标
+
+# Use multiple threads to compile targets in parallel
 threads = []
 for target in targets:
     thread = threading.Thread(target=compile_target, args=(target,))
@@ -32,26 +35,30 @@ for target in targets:
 for thread in threads:
     thread.join()
 
-# 定义函数来处理目标文件
+# Define a function to process target files
+
+
 def process_target(target):
     game_name = target[0]
     out_dir = f"./out/{game_name}"
     tmp_dir = f"./tmp/{game_name}"
 
-    # 创建输出目录
+    # Create the output directory
     os.makedirs(out_dir, exist_ok=True)
 
-    # 复制文件
-    shutil.copy(f"{tmp_dir}/release/FallrimPriority.dll", f"{out_dir}/FallrimPriority.dll")
+    # Copy files
+    shutil.copy(f"{tmp_dir}/release/FallrimPriority.dll",
+                f"{out_dir}/FallrimPriority.dll")
     shutil.copy("./FallrimPriority.ini", f"{out_dir}/FallrimPriority.ini")
 
-    # 压缩目录
+    # Compress the directory
     shutil.make_archive(out_dir, "zip", root_dir=out_dir)
 
-    # 删除目录
+    # Remove the directory
     shutil.rmtree(out_dir)
 
-# 多线程并行遍历数组来处理目标文件
+
+# Use multiple threads to process target files in parallel
 threads = []
 for target in targets:
     thread = threading.Thread(target=process_target, args=(target,))
@@ -60,5 +67,8 @@ for target in targets:
 
 for thread in threads:
     thread.join()
+
+# Delete "./tmp" folder
+shutil.rmtree("./tmp")
 
 print("Finished")
